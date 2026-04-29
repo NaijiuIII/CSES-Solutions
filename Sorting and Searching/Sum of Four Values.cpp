082b88d65691ee138c95755f9dceb3b5815bde5f
NaijiuIII <1309029084@qq.com>
2026-04-29 20:53:18 +0800

    Sum of Four Values.cpp

---
+++ Sorting and Searching/Sum of Four Values.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    cin>>n;
+    long long s;
+    cin>>s;
+
+    vector<pair<long long,int>> a(n);
+    for (int i=0;i<n;i++){
+        cin>>a[i].first;
+        a[i].second=i+1;
+    }
+
+    sort(a.begin(),a.end());
+    bool found=false;
+
+    vector<int> ans;
+
+    for (int i=0;i<n;i++){
+        if (found) break;
+        for (int j=i+1;j<n;j++){
+            int l=j+1,r=n-1;
+            if (found) break;
+            while (l<r){
+
+                if (found) break;
+
+                long long k=a[i].first+a[l].first+a[r].first+a[j].first;
+                if (k==s){
+                    found=true;
+                    ans.push_back(a[i].second);
+                    ans.push_back(a[l].second);
+                    ans.push_back(a[r].second);
+                    ans.push_back(a[j].second);
+                }
+                else if (k<s) l++;
+                else r--;
+            }
+        }
+    }
+
+    if (found){
+        for (int i=0;i<ans.size();i++){
+            if (i) cout<<' ';
+            cout<<ans[i];
+        }
+    }
+    else cout<<"IMPOSSIBLE";
+    cout<<'\n';
+    return 0; 
+}
