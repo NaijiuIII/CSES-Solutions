2e99379420db7ae3eb6b5ef15c36a6bb9c6ae8ab
NaijiuIII <1309029084@qq.com>
2026-05-03 14:54:18 +0800

    Add Coin Combinations II

---
+++ Dynamic Programming/Coin Combinations II.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+ 
+    int n;
+    long long x;
+    cin>>n>>x;
+ 
+    long long mod=1e9+7;
+ 
+    vector<long long> c(n);
+    for (int i=0;i<n;i++) cin>>c[i];
+    sort(c.begin(),c.end());
+ 
+    vector<long long> dp(x+1,0);
+    dp[0]=1;
+ 
+    for (int j:c){
+        for (int i=1;i<=x;i++){
+            if (i-j>=0){
+                dp[i]=(dp[i]+dp[i-j])%mod;
+            }
+        }
+    }
+ 
+    cout<<dp[x]<<'\n';
+    return 0;
+}
