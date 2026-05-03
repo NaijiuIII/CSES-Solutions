49f5197b40a9911c78c4ffd14d3c9a659a033281
NaijiuIII <1309029084@qq.com>
2026-05-03 14:50:44 +0800

    Add Dice Combinations

--- Dynamic Programming/Dice Combinations.cpp
+++ Dynamic Programming/Dice Combinations.cpp
@@ -1,35 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
  
-
-
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-
-    int n,a,b;
-    cin>>n>>a>>b;
-
-    vector<long long> x(n);
-    for (int i=0;i<n;i++){
-        cin>>x[i];
-    }
-
-    vector<long long> pre(n+1);
-    pre[0]=0;
-    for (int i=1;i<=n;i++) pre[i]=pre[i-1]+x[i-1];
-
-    multiset<long long> s;
-    long long ans=-4e18;
-    for (int r=a;r<=n;r++){
-        s.insert(pre[r-a]);
-        if (r-b-1>=0){
-            s.erase(s.find(pre[r-b-1]));
+ 
+    int n;
+    cin>>n;
+ 
+    long long mod=1e9+7;
+ 
+    vector<long long> dp(n+1,0);
+    dp[0]=1;
+    for (int i=1;i<=n;i++){
+        for (int j=i-6;j<i;j++){
+            if (j<0) continue;
+            dp[i]=(dp[i]+dp[j])%mod;
         }
-
-        ans=max(ans,pre[r]-*s.begin());
     }
-
-    cout<<ans<<'\n';
-    return 0; 
+    cout<<dp[n]<<'\n';
+    return 0;
 }
